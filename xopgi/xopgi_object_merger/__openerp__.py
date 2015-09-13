601aab7dedcdcad11672684b044b93b8bcc9045b
Yoany <yoany.r@gmail.com>
2015-09-13 16:15:13 -0400

    Extend object_merger to define merge ways for models and fields.

--- xopgi/xopgi_object_merger/__openerp__.py
+++ xopgi/xopgi_object_merger/__openerp__.py
@@ -25,6 +25,8 @@
         "view/res_config_view.xml",
         "view/object_merger_view.xml",
         "data/know_informal_reference.xml",
+        "data/predefine_field_merge_way.xml",
+
         "security/security.xml",
     ],
     'demo': [],
