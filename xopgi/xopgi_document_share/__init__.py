7afac1e6cb57730c3a78d16518742c00892a9ccf
carlos.abel <carlos.abel@merchise.org>
2015-10-11 01:59:21 -0400

    Update module

---
+++ xopgi/xopgi_document_share/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+# ---------------------------------------------------------------------
+# xopgi_show_documents
+# ---------------------------------------------------------------------
+# Copyright (c) 2015 Merchise Autrement and Contributors
+# All rights reserved.
+#
+# This is free software; you can redistribute it and/or modify it under the
+# terms of the LICENCE attached (see LICENCE file) in the distribution
+# package.
+#
+
+
+import document_share
+
+
