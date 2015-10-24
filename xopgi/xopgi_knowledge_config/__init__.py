df488fd1eadf223dd8c32989b3cb27f7b8fcfdf6
carlos.abel <carlos.abel@merchise.org>
2015-10-24 08:27:41 -0400

    Addon to config knowledge

---
+++ xopgi/xopgi_knowledge_config/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+# ---------------------------------------------------------------------
+# xopgi_knowledge_config
+# ---------------------------------------------------------------------
+# Copyright (c) 2015 Merchise Autrement and Contributors
+# All rights reserved.
+#
+# This is free software; you can redistribute it and/or modify it under the
+# terms of the LICENCE attached (see LICENCE file) in the distribution
+# package.
+#
+
+
+import res_config
+
