6e56ac7893132d46a85232cd21d48cd1c3ea0484
Yoany <yoany.r@gmail.com>
2015-11-01 15:00:28 -0500

    Change action managing to a generic way.

--- xopgi/xopgi_board/static/src/js/xopgi_board.js
+++ xopgi/xopgi_board/static/src/js/xopgi_board.js
@@ -1,5 +1,6 @@
 openerp.xopgi_board = function(instance) {
-var QWeb = instance.web.qweb;
+var QWeb = instance.web.qweb,
+    Model = instance.web.Model;
 
 instance.xopgi_board = instance.xopgi_board || {};
 
@@ -16,6 +17,8 @@ instance.web.form.XopgiBoard = instance.web.form.FormWidget.extend({
         // Events
         this.$el.delegate('.oe_board_container .oe_fold',
                           'click', this.on_fold_action);
+        this.$el.delegate('.o_dashboard_action', 'click',
+                          this.on_dashboard_action_clicked);
     },
 
     fetch_data: function () {
@@ -55,6 +58,62 @@ instance.web.form.XopgiBoard = instance.web.form.FormWidget.extend({
             id = parseInt($action.attr('data-id'), 10);
         $e.toggleClass('oe_minimize oe_maximize');
         $action.find('.oe_board_widget_content').toggle();
+    },
+
+    on_dashboard_action_clicked: function (ev) {
+        ev.preventDefault();
+
+        var self = this;
+        var $action = $(ev.currentTarget);
+        var action_name = $action.attr('name');
+        var options = {
+            action_menu_id: $action.data('menu_id') || null,
+            additional_context: instance.web.pyeval.eval(
+                'context', $action.data('context'), {}) || {},
+        };
+        new Model("ir.model.data")
+            .call("xmlid_to_res_id", [action_name])
+            .then(function (data) {
+                if (data) {
+                    self.do_action(data, options);
+                }
+            });
+    },
+
+    render_monetary_field: function (value, currency_id) {
+        var self = this;
+        this.currency = false;
+        this.get_currency_info(currency_id);
+        if (this.currency) {
+            var digits_precision = [69, this.currency.decimal_places || 2];
+            value = instance.web.format_value(value || 0, {
+                type: "float",
+                digits: digits_precision
+            });
+            if (this.currency.position === "after") {
+                value += this.currency.symbol;
+            } else {
+                value = this.currency.symbol + value;
+            }
+        }
+        return value
+    },
+
+    get_currency_info: function (currency_id) {
+        this.currency = {
+            symbol: '$',
+            position: 'after',
+            decimal_places: 2
+        };
+        var self = this;
+        if (currency_id) {
+            new Model("res.currency").query(
+                ["symbol", "position", "decimal_places"])
+                .filter([["id", "=", currency_id]]).first()
+                .then(function (result) {
+                    self.currency = result ? result : self.currency;
+                });
+        }
     },
 });
 
--- xopgi/xopgi_sale_board/static/src/js/xopgi_sale_board.js
+++ xopgi/xopgi_sale_board/static/src/js/xopgi_sale_board.js
@@ -5,42 +5,7 @@ var Model = instance.web.Model;
 
 instance.web.form.XopgiBoard.include({
     events: {
-        'click .o_dashboard_action': 'on_dashboard_action_clicked',
         'click .o_target_to_set': 'on_dashboard_target_clicked',
-    },
-
-
-    on_dashboard_action_clicked: function (ev) {
-        ev.preventDefault();
-
-        var self = this;
-        var $action = $(ev.currentTarget);
-        var action_name = $action.attr('name');
-        var action_extra = $action.data('extra');
-        var additional_context = {}
-
-        // TODO: find a better way to add defaults to search view
-        if (action_name === 'calendar.action_calendar_event') {
-            additional_context['search_default_mymeetings'] = 1;
-        } else if (action_name === 'xopgi_sale_board.crm_lead_action_activities') {
-            if (action_extra === 'today') {
-                additional_context['search_default_today'] = 1;
-            } else if (action_extra === 'this_week') {
-                additional_context['search_default_this_week'] = 1;
-            } else if (action_extra === 'overdue') {
-                additional_context['search_default_overdue'] = 1;
-            }
-        } else if (action_name === 'crm.crm_opportunity_report_action_graph') {
-            additional_context['search_default_won'] = 1;
-        }
-
-        new Model("ir.model.data")
-            .call("xmlid_to_res_id", [action_name])
-            .then(function (data) {
-                if (data) {
-                    self.do_action(data, {additional_context: additional_context});
-                }
-            });
     },
 
     on_change_input_target: function (e) {
@@ -99,42 +64,6 @@ instance.web.form.XopgiBoard.include({
                 .focus()
                 .select();
         });
-    },
-
-    render_monetary_field: function (value, currency_id) {
-        var self = this;
-        this.currency = false;
-        this.get_currency_info(currency_id);
-        if (this.currency) {
-            var digits_precision = [69, this.currency.decimal_places || 2];
-            value = instance.web.format_value(value || 0, {
-                type: "float",
-                digits: digits_precision
-            });
-            if (this.currency.position === "after") {
-                value += this.currency.symbol;
-            } else {
-                value = this.currency.symbol + value;
-            }
-        }
-        return value
-    },
-
-    get_currency_info: function (currency_id) {
-        this.currency = {
-            symbol: '$',
-            position: 'after',
-            decimal_places: 2
-        };
-        var self = this;
-        if (currency_id){
-            new Model("res.currency").query(
-                ["symbol", "position", "decimal_places"])
-                .filter([["id", "=", currency_id]]).first()
-                .then(function (result) {
-                    self.currency = result ? result : self.currency;
-                });
-        }
     },
 });
 
