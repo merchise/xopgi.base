ddb9e6db4937e12c6b7ec97827986239a4fe7840
Monica Diaz Pena <monicadp@merchise.org>
2016-02-04 17:30:12 -0500

    Adding new widget for many2one fields (many2one_default_choices).

---
+++ xopgi/widget_m2o_default_choices/__init__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+# ---------------------------------------------------------------------
+# __init__
+# ---------------------------------------------------------------------
+# Copyright (c) 2016 Merchise Autrement and Contributors
+# All rights reserved.
+#
+# This is free software; you can redistribute it and/or modify it under the
+# terms of the LICENCE attached (see LICENCE file) in the distribution
+# package.
+#
+# Created on 2016-02-04
+
+'''doc
+
+'''
+
+from __future__ import (division as _py3_division,
+                        print_function as _py3_print,
+                        absolute_import as _py3_abs_import)
