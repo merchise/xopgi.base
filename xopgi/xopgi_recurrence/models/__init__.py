f5c78cc32739a704f907e8a63be06d2a854997df
Johanny <johanny@merchise.org>
2016-05-12 18:10:09 -0400

    Add xopgi_recurrence module.

---
+++ xopgi/xopgi_recurrence/models/__init__.py
@@ -0,0 +1,18 @@
+# -*- encoding: utf-8 -*-
+# ---------------------------------------------------------------------
+# xopgi_recurrence.models
+# ---------------------------------------------------------------------
+# Copyright (c) 2016 Merchise Autrement and Contributors
+# All rights reserved.
+#
+# This is free software; you can redistribute it and/or modify it under the
+# terms of the LICENCE attached (see LICENCE file) in the distribution
+# package.
+#
+# Created on 2016-05-12
+
+from __future__ import (division as _py3_division,
+                        print_function as _py3_print,
+                        absolute_import as _py3_abs_import)
+
+from . import recurrent_model  # noqa
