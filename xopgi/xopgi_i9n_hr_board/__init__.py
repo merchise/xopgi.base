4b84915ea1bc7d634cd2b9a7b057c98dc9ca0bff
Oraldo Jacinto Simón <oraldo@merchise.org>
2017-07-31 22:01:52 -0400

    Introduce addons ``xopgi_i9n_hr_board``.

---
+++ xopgi/xopgi_i9n_hr_board/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# ---------------------------------------------------------------------
+# xopgi_board_widget
+# ---------------------------------------------------------------------
+# Copyright (c) 2017 Merchise Autrement [~º/~] and Contributors
+# All rights reserved.
+#
+# This is free software; you can redistribute it and/or modify it under the
+# terms of the LICENCE attached (see LICENCE file) in the distribution
+# package.
+#
+# @created: 2017-07-31
+
+from xoeuf import MAJOR_ODOO_VERSION
