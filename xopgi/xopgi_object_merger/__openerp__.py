c76b2de346854a92a65156199635aac5e5631404
Manuel Vázquez Acosta <manu@merchise.org>
2017-08-07 22:04:06 -0400

    Use MAJOR_ODOO_VERSION.

--- xopgi/xopgi_object_merger/__openerp__.py
+++ xopgi/xopgi_object_merger/__openerp__.py
@@ -30,7 +30,7 @@
 
     # MIGRATION POLICY: All addons are not included until someone work on them
     # and upgrade them.
-    'installable': (8, 0) <= ODOO_VERSION_INFO < (9, 0),   # noqa
+    'installable': 8 <= MAJOR_ODOO_VERSION < 9,   # noqa
 
     'application': True,
 }
