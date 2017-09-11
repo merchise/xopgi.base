2bf25694879d4424dd2f2156cc3f968615c7d4f7
Manuel Vázquez Acosta <manu@merchise.org>
2017-09-11 12:56:49 -0400

    Remove usage of simplejson.
    We don't require 'simplejson' in our setup.py.  It was installed because Odoo
    8 requires it.  But Odoo 9 does not require it.

--- xopgi/xopgi_board/res_currency.py
+++ xopgi/xopgi_board/res_currency.py
@@ -14,8 +14,7 @@ from __future__ import (division as _py3_division,
                         print_function as _py3_print,
                         absolute_import as _py3_abs_import)
 
-import simplejson as json
-
+import json
 from xoeuf import api, models
 
 
